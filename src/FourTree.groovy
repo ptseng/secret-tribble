b0a51d25f0dda0a7d89a9c63d594dabd97b937ae
Matt Clyde <mclyde@pdx.edu>
2014-02-16 17:45:24 -0800

    Added FourTree class

---
+++ src/FourTree.groovy
@@ -0,0 +1,23 @@
+import java.lang.*
+import java.util.*
+
+// FourTree is a 2-3-4 Tree using integer keys
+class FourTree {
+
+    public static void main ( String [] args ) {
+
+    }
+
+    FourTree ( ) {
+
+    }
+
+    public void insert ( ) {
+
+    }
+
+    public void search ( ) {
+
+    }
+
+}
