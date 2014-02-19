84ec6746389718d22b8b2cba97315c3753bf27fa
Matt Clyde <mclyde@pdx.edu>
2014-02-18 22:13:29 -0800

    FourTreeNode uses generics

--- src/FourTreeNode.groovy
+++ src/FourTreeNode.groovy
@@ -1,16 +1,20 @@
 
 
-// Node for 2-3-4 tree in which keys are Data objects
-class FourTreeNode {
+// Node for 2-3-4 tree in which keys are Data object
+class FourTreeNode<K,V> {
 
-    private def Data key1, key2, key3;
-    private def FourTreeNode left, leftmid, rightmid, right;
+    private def Data<K,V> key1, key2, key3;
+    private def FourTreeNode<K,V> left, leftmid, rightmid, right;
 
     public static void main ( String [] args ) {
 
     }
 
-    public FourTreeNode ( k1 = -1, k2 = -1, k3 = -1, l = null, lm = null, rm = null, r = null ) {
+    public FourTreeNode ( Data<K,V> k ) {
+        key2 = k
+    }
+
+    public FourTreeNode ( k1 = null, k2 = null, k3 = null, l = null, lm = null, rm = null, r = null ) {
         key1 = k1
         key2 = k2
         key3 = k3
@@ -29,7 +33,7 @@ class FourTreeNode {
         }
     }
 
-    def getData ( k ) {
+    def getValue ( k ) {
         switch ( k ) {
             case 1: return key1.getValue()
             case 2: return key2.getValue()
@@ -38,7 +42,7 @@ class FourTreeNode {
         }
     }
 
-    def setKey ( k, key ) {
+    def setKeyValue ( k, key ) {
         switch ( k ) {
             case 1: return key1.setKey( key )
             case 2: return key2.setKey( key )
@@ -54,6 +58,22 @@ class FourTreeNode {
             case 3: return key3.setValue( value )
             default: return
         }
+    }
+
+    def setData ( k, d ) {
+        switch ( k ) {
+            case 1: return key1 = d
+            case 2: return key2 = d
+            case 3: return key3 = d
+            default: return
+        }
+    }
+
+    def isLeaf () {
+        if ( left == null && leftmid == null && rightmid == null && right == null ) {
+            return true
+        }
+        false
     }
 
 }
