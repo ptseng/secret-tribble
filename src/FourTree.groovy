4dba61face17625d470d6f8f67678c83545b5e98
Matt Clyde <mclyde@pdx.edu>
2014-02-22 19:55:14 -0800

    Added insert() function for Collections

--- src/FourTree.groovy
+++ src/FourTree.groovy
@@ -12,21 +12,38 @@ class FourTree<K,V> {
         FourTree tree2 = new FourTree<Tuple, String>()
         FourTree tree3 = new FourTree<String,String>()
 
+        def dataSet = new HashSet<Data<String,String>>()
+
         def data = new Data<String,String>("ggg","jee")
         tree1.insert(data)
+        dataSet.add(data)
         data = new Data<String,String>("xxx","exs")
         tree1.insert(data)
+        dataSet.add(data)
         data = new Data<String,String>("bbb","bee")
         tree1.insert(data)
+        dataSet.add(data)
         data = new Data<String,String>("fff","efffff")
         tree1.insert(data)
+        dataSet.add(data)
         data = new Data<String,String>("zzz","snore")
         tree1.insert(data)
+        dataSet.add(data)
         data = new Data<String,String>("hhh","laugh at me")
         tree1.insert(data)
+        dataSet.add(data)
 
-        /*def dataSet = new HashSet<Data<String,String>>()
-        Random randomVal = new Random()
+        data = new Data<String,String>("ttt","scold")
+        dataSet.add(data)
+        data = new Data<String,String>("oooooo","awe")
+        dataSet.add(data)
+        data = new Data<String,String>("ttt","scold")
+        dataSet.add(data)
+        data = new Data<String,String>("nnnn","munch food")
+        dataSet.add(data)
+        tree3.insert(dataSet)
+
+        /*Random randomVal = new Random()
         for( i in 0..100 ) {
             StringBuilder key = new StringBuilder()
             StringBuilder value = new StringBuilder()
@@ -46,7 +63,11 @@ class FourTree<K,V> {
 
         tree1.printInOrder()
         println()
+        tree3.printInOrder()
+        println()
         println tree1.find("ggg")
+        println()
+        println tree3.find("hhh")
 
     }
 
@@ -128,7 +149,7 @@ class FourTree<K,V> {
 
     }
 
-    def insert ( data ) {
+    def insert ( Data<K,V> data ) {
 
         if( root == null ) {
             return root = new FourTreeNode( data )
@@ -148,6 +169,12 @@ class FourTree<K,V> {
 
         add( data, root )
 
+    }
+
+    def insert ( Collection<Data> dataSet ) {
+        for ( i in dataSet ) {
+            insert( i )
+        }
     }
 
     def private add ( data, rt ) {
