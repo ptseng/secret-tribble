dadfb246c245916026e33a1e225c7a59b8518261
Matt Clyde <mclyde@pdx.edu>
2014-03-01 11:16:27 -0800

    Began random string generator

---
+++ src/RandomStringGen.groovy
@@ -0,0 +1,22 @@
+import org.apache.commons.lang.*
+
+static class RandomStringGen {
+
+    def generateString( length, letters, numbers ) {
+
+        def randString = RandomStringUtils.random( length, letters, numbers )
+
+    }
+
+    def generateMap(  ) {
+
+        def randSet = new HashMap<String,String>()
+
+
+    }
+
+    public static void main( String [] args ) {
+
+    }
+
+}
